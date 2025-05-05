d872b238494598e84f1f728cb3dee30f3fb25507
PatrickBriane <202411119@gordoncollege.edu.ph>
2025-05-06 00:52:41 +0800

    Functional Version 1.2 (Organized)

--- scripts/new arrivals.js
+++ scripts/new arrivals.js
@@ -1,46 +1,56 @@
-const products = [{
-    image: 'Images/Shoes/Jogger Lite Black 2.png',
-    brand: 'Reebok',
-    name: 'Jogger Lite Black',
-    price: '₱1,090',
+const products = [
+  {
+    image: "Images/Shoes/Jogger Lite Black 2.png",
+    brand: "Reebok",
+    name: "Jogger Lite Black",
+    price: "₱1,090",
     priceValue: 1090,
-    isNew: true
-}, {
-    image: 'Images/Shoes/Jogger Lite Burgundy2.png',
-    brand: 'Reebok',
-    name: 'Jogger Lite Burgundy',
-    price: '₱1,090',
+    isNew: true,
+  },
+  {
+    image: "Images/Shoes/Jogger Lite Burgundy2.png",
+    brand: "Reebok",
+    name: "Jogger Lite Burgundy",
+    price: "₱1,090",
     pricevalue: 1090,
-    isNew: true
-}, {
-    image: 'Images/Shoes/Stridium 2.0 “Grey”.png',
-    brand: 'Reebok',
-    name: 'Stridium 2.0 “Grey”',
-    price: '₱1,790',
+    isNew: true,
+  },
+  {
+    image: "Images/Shoes/Stridium 2.0 “Grey”.png",
+    brand: "Reebok",
+    name: "Stridium 2.0 “Grey”",
+    price: "₱1,790",
     pricevalue: 1790,
-    isNew: true
-}, {
-    image: 'Images/Shoes/Stridium 2.0 “Black”.png',
-    brand: 'Reebok',
-    name: 'Stridium 2.0 “Black”',
-    price: '₱990',
+    isNew: true,
+  },
+  {
+    image: "Images/Shoes/Stridium 2.0 “Black”.png",
+    brand: "Reebok",
+    name: "Stridium 2.0 “Black”",
+    price: "₱990",
     pricevalue: 990,
-    isNew: true
-}, {
-    image: 'Images/Shoes/Nike Fly.By Mid 3 BlackWhite.png',
-    brand: 'Nike',
-    name: 'Nike Fly.By Mid 3 Black/White',
-    price: '₱1,090',
+    isNew: true,
+  },
+  {
+    image: "Images/Shoes/Nike Fly.By Mid 3 BlackWhite.png",
+    brand: "Nike",
+    name: "Nike Fly.By Mid 3 Black/White",
+    price: "₱1,090",
     pricevalue: 1090,
-    isNew: true
-}];
+    isNew: true,
+  },
+];
 
-let productsHTML = '';
+let productsHTML = "";
 
 products.forEach((product) => {
-    productsHTML += `
-     <div class="product-wrapper" data-brand="${product.brand}" data-new="${product.isNew}">
-            <a href="shoe.html" class="js-product-link" data-product='${JSON.stringify(product)}'>
+  productsHTML += `
+     <div class="product-wrapper" data-brand="${product.brand}" data-new="${
+    product.isNew
+  }">
+            <a href="shoe.html" class="js-product-link" data-product='${JSON.stringify(
+              product
+            )}'>
                 <div class="container">
                     <div class="shoe" style="background-color: #DFDFDF;">
                         <img class="shoe-img" src="${product.image}" alt="">
@@ -69,49 +79,51 @@ products.forEach((product) => {
     `;
 });
 
-document.querySelector('.js-product-area').innerHTML = productsHTML;
+document.querySelector(".js-product-area").innerHTML = productsHTML;
 
-document.querySelectorAll('.brand-filter').forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        const selectedBrand = link.dataset.brand;
+document.querySelectorAll(".brand-filter").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const selectedBrand = link.dataset.brand;
 
-        document.querySelectorAll('.product-wrapper').forEach(product => {
-            const productBrand = product.dataset.brand;
-            product.style.display = (selectedBrand === "All" || productBrand === selectedBrand) ? "flex" : "none";
-        });
-
-        updateIntro(link.dataset.title);
+    document.querySelectorAll(".product-wrapper").forEach((product) => {
+      const productBrand = product.dataset.brand;
+      product.style.display =
+        selectedBrand === "All" || productBrand === selectedBrand
+          ? "flex"
+          : "none";
     });
+
+    updateIntro(link.dataset.title);
+  });
 });
 
-document.querySelector('.new-arrivals-filter').addEventListener('click', e => {
+document
+  .querySelector(".new-arrivals-filter")
+  .addEventListener("click", (e) => {
     e.preventDefault();
 
-    document.querySelectorAll('.product-wrapper').forEach(product => {
-        const isNew = product.dataset.new === 'true';
-        product.style.display = isNew ? 'flex' : 'none';
+    document.querySelectorAll(".product-wrapper").forEach((product) => {
+      const isNew = product.dataset.new === "true";
+      product.style.display = isNew ? "flex" : "none";
     });
 
     updateIntro(e.target.dataset.title);
-});
-
-
-function updateIntro(title) {
-    const intro = document.getElementById('intro');
-    intro.querySelector('h1').textContent = title;
-}
-
-document.querySelectorAll('.js-product-link').forEach(link => {
-    link.addEventListener('click', e => {
-        const productData = e.currentTarget.dataset.product;
-        localStorage.setItem('selectedProduct', productData);
-    });
-});
-
-shoeCard.addEventListener('click', () => {
-    localStorage.setItem('selectedProduct', JSON.stringify(product));
-    window.location.href = 'Shoe.html'; 
   });
 
-  
+function updateIntro(title) {
+  const intro = document.getElementById("intro");
+  intro.querySelector("h1").textContent = title;
+}
+
+document.querySelectorAll(".js-product-link").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    const productData = e.currentTarget.dataset.product;
+    localStorage.setItem("selectedProduct", productData);
+  });
+});
+
+shoeCard.addEventListener("click", () => {
+  localStorage.setItem("selectedProduct", JSON.stringify(product));
+  window.location.href = "Shoe.html";
+});
--- scripts/products.js
+++ scripts/products.js
@@ -1,198 +1,227 @@
-const products = [{
-    image: 'Images/Shoes/Jogger Lite Black 2.png',
-    brand: 'Reebok',
-    name: 'Jogger Lite Black',
-    price: '₱1,090',
+const products = [
+  {
+    image: "Images/Shoes/Jogger Lite Black 2.png",
+    brand: "Reebok",
+    name: "Jogger Lite Black",
+    price: "₱1,090",
     priceValue: 1090,
-    isNew: true
-}, {
-    image: 'Images/Shoes/Jogger Lite Burgundy2.png',
-    brand: 'Reebok',
-    name: 'Jogger Lite Burgundy',
-    price: '₱1,090',
+    isNew: true,
+  },
+  {
+    image: "Images/Shoes/Jogger Lite Burgundy2.png",
+    brand: "Reebok",
+    name: "Jogger Lite Burgundy",
+    price: "₱1,090",
     pricevalue: 1090,
-    isNew: true
-}, {
-    image: 'Images/Shoes/Stridium 2.0 “Grey”.png',
-    brand: 'Reebok',
-    name: 'Stridium 2.0 “Grey”',
-    price: '₱1,790',
+    isNew: true,
+  },
+  {
+    image: "Images/Shoes/Stridium 2.0 “Grey”.png",
+    brand: "Reebok",
+    name: "Stridium 2.0 “Grey”",
+    price: "₱1,790",
     pricevalue: 1790,
-    isNew: true
-}, {
-    image: 'Images/Shoes/Stridium 2.0 “Black”.png',
-    brand: 'Reebok',
-    name: 'Stridium 2.0 “Black”',
-    price: '₱990',
+    isNew: true,
+  },
+  {
+    image: "Images/Shoes/Stridium 2.0 “Black”.png",
+    brand: "Reebok",
+    name: "Stridium 2.0 “Black”",
+    price: "₱990",
     pricevalue: 990,
-    isNew: true
-}, {
-    image: 'Images/Shoes/Nike Fly.By Mid 3 BlackWhite.png',
-    brand: 'Nike',
-    name: 'Nike Fly.By Mid 3 Black/White',
-    price: '₱1,090',
+    isNew: true,
+  },
+  {
+    image: "Images/Shoes/Nike Fly.By Mid 3 BlackWhite.png",
+    brand: "Nike",
+    name: "Nike Fly.By Mid 3 Black/White",
+    price: "₱1,090",
     pricevalue: 1090,
-    isNew: true
-}, {
-    image: 'Images/Shoes/Jordan 38 “White Metallic Gold”.png',
-    detailImage: 'Images/Shoes/7 1 (1).png',
-    brand: 'Nike',
-    name: 'Jordan 38 “White Metallic Gold”',
-    price: '₱3,890',
+    isNew: true,
+  },
+  {
+    image: "Images/Shoes/Jordan 38 “White Metallic Gold”.png",
+    detailImage: "Images/Shoes/7 1 (1).png",
+    brand: "Nike",
+    name: "Jordan 38 “White Metallic Gold”",
+    price: "₱3,890",
     pricevalue: 3890,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Nike Impact 3 “Black Grey”2.png',
-    detailImage: 'Images/Shoes/6 1 (2).png',
-    brand: 'Nike',
-    name: 'Nike Impact 3 “Black Grey”',
-    price: '₱1,690',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Nike Impact 3 “Black Grey”2.png",
+    detailImage: "Images/Shoes/6 1 (2).png",
+    brand: "Nike",
+    name: "Nike Impact 3 “Black Grey”",
+    price: "₱1,690",
     pricevalue: 1690,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Nike KD Trey 5 X “Green Strike”.png',
-    detailImage: 'Images/Shoes/8 1 (2).png',
-    brand: 'Nike',
-    name: 'Nike KD Trey 5 X “Green Strike”',
-    price: '₱1,790',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Nike KD Trey 5 X “Green Strike”.png",
+    detailImage: "Images/Shoes/8 1 (2).png",
+    brand: "Nike",
+    name: "Nike KD Trey 5 X “Green Strike”",
+    price: "₱1,790",
     pricevalue: 1790,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Giannis Immortality 3 “Photo Blue”.png',
-    detailImage: 'Images/Shoes/2 1 (1).png',
-    brand: 'Nike',
-    name: 'Giannis Immortality 3 “Photo Blue”',
-    price: '₱2,290',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Giannis Immortality 3 “Photo Blue”.png",
+    detailImage: "Images/Shoes/2 1 (1).png",
+    brand: "Nike",
+    name: "Giannis Immortality 3 “Photo Blue”",
+    price: "₱2,290",
     pricevalue: 2290,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Giannis Immortality 4 “Halloween”.png',
-    detailImage: 'Images/Shoes/4 1 (1).png',
-    brand: 'Nike',
-    name: 'Giannis Immortality 4 “Halloween”',
-    price: '₱2,690',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Giannis Immortality 4 “Halloween”.png",
+    detailImage: "Images/Shoes/4 1 (1).png",
+    brand: "Nike",
+    name: "Giannis Immortality 4 “Halloween”",
+    price: "₱2,690",
     pricevalue: 2690,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Freak 4 “Indigo Haze”.png',
-    detailImage: 'Images/Shoes/6 2 (1).png',
-    brand: 'Nike',
-    name: 'Freak 4 “Indigo Haze”',
-    price: '₱3,590',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Freak 4 “Indigo Haze”.png",
+    detailImage: "Images/Shoes/6 2 (1).png",
+    brand: "Nike",
+    name: "Freak 4 “Indigo Haze”",
+    price: "₱3,590",
     pricevalue: 3590,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Nike G.T Cut 3 “Asheen Slate.png',
-    detailImage: 'Images/Shoes/8 2 (1).png',
-    brand: 'Nike',
-    name: 'Nike G.T Cut 3 “Asheen Slate',
-    price: '₱4,990',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Nike G.T Cut 3 “Asheen Slate.png",
+    detailImage: "Images/Shoes/8 2 (1).png",
+    brand: "Nike",
+    name: "Nike G.T Cut 3 “Asheen Slate",
+    price: "₱4,990",
     pricevalue: 4990,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Nike Elevate 3 “White Green”.png',
-    detailImage: 'Images/Shoes/12 1 (1).png',
-    brand: 'Nike',
-    name: 'Nike Elevate 3 “White Green”',
-    price: '₱1,490',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Nike Elevate 3 “White Green”.png",
+    detailImage: "Images/Shoes/12 1 (1).png",
+    brand: "Nike",
+    name: "Nike Elevate 3 “White Green”",
+    price: "₱1,490",
     pricevalue: 1490,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Jordan Zion 3 “Pressure”.png',
-    detailImage: 'Images/Shoes/16 1 (1).png',
-    brand: 'Nike',
-    name: 'Jordan Zion 3 “Pressure”',
-    price: '₱2,990',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Jordan Zion 3 “Pressure”.png",
+    detailImage: "Images/Shoes/16 1 (1).png",
+    brand: "Nike",
+    name: "Jordan Zion 3 “Pressure”",
+    price: "₱2,990",
     pricevalue: 2990,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Nike Cortez “Sail Stadium Green”.png',
-    detailImage: 'Images/Shoes/2 2 (1).png',
-    brand: 'Nike',
-    name: 'Nike Cortez “Sail Stadium Green”',
-    price: '₱2,290',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Nike Cortez “Sail Stadium Green”.png",
+    detailImage: "Images/Shoes/2 2 (1).png",
+    brand: "Nike",
+    name: "Nike Cortez “Sail Stadium Green”",
+    price: "₱2,290",
     pricevalue: 2290,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Nike Dunk Low “Panda”.png',
-    detailImage: 'Images/Shoes/4 2 (1).png',
-    brand: 'Nike',
-    name: 'Nike Dunk Low “Panda”',
-    price: '₱4,590',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Nike Dunk Low “Panda”.png",
+    detailImage: "Images/Shoes/4 2 (1).png",
+    brand: "Nike",
+    name: "Nike Dunk Low “Panda”",
+    price: "₱4,590",
     pricevalue: 4590,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Nike AirForce 1 Low “Black Anthracite”.png',
-    detailImage: 'Images/Shoes/34 1 (1).png',
-    brand: 'Nike',
-    name: 'Nike AirForce 1 Low “Black Anthracite”',
-    price: '₱1,990',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Nike AirForce 1 Low “Black Anthracite”.png",
+    detailImage: "Images/Shoes/34 1 (1).png",
+    brand: "Nike",
+    name: "Nike AirForce 1 Low “Black Anthracite”",
+    price: "₱1,990",
     pricevalue: 1990,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Jordan 1 Take 5 “Bred”.png',
-    detailImage: 'Images/Shoes/10 3 (1).png',
-    brand: 'Nike',
-    name: 'Jordan 1 Take 5 “Bred”',
-    price: '₱2,590',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Jordan 1 Take 5 “Bred”.png",
+    detailImage: "Images/Shoes/10 3 (1).png",
+    brand: "Nike",
+    name: "Jordan 1 Take 5 “Bred”",
+    price: "₱2,590",
     pricevalue: 2590,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Nike SuperRep Go 3 Next Nature.png',
-    detailImage: 'Images/Shoes/26 1 (1).png',
-    brand: 'Nike',
-    name: 'Nike SuperRep Go 3 Next Nature',
-    price: '₱1,790',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Nike SuperRep Go 3 Next Nature.png",
+    detailImage: "Images/Shoes/26 1 (1).png",
+    brand: "Nike",
+    name: "Nike SuperRep Go 3 Next Nature",
+    price: "₱1,790",
     pricevalue: 1790,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Nike Air Max Solo.png',
-    detailImage: 'Images/Shoes/42 1 (1).png',
-    brand: 'Nike',
-    name: 'Nike Air Max Solo',
-    price: '₱1,990',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Nike Air Max Solo.png",
+    detailImage: "Images/Shoes/42 1 (1).png",
+    brand: "Nike",
+    name: "Nike Air Max Solo",
+    price: "₱1,990",
     pricevalue: 1990,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Nike AirForce 1 “Year of the Dragon”.png',
-    detailImage: 'Images/Shoes/30 1 (1).png',
-    brand: 'Nike',
-    name: 'Nike AirForce 1 “Year of the Dragon”',
-    price: '₱1,990',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Nike AirForce 1 “Year of the Dragon”.png",
+    detailImage: "Images/Shoes/30 1 (1).png",
+    brand: "Nike",
+    name: "Nike AirForce 1 “Year of the Dragon”",
+    price: "₱1,990",
     pricevalue: 1990,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Nike AirForce 1 Low “Heel Stitch”.png',
-    detailImage: 'Images/Shoes/24 1 (1).png',
-    brand: 'Nike',
-    name: 'Nike AirForce 1 Low “Heel Stitch”',
-    price: '₱1,990',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Nike AirForce 1 Low “Heel Stitch”.png",
+    detailImage: "Images/Shoes/24 1 (1).png",
+    brand: "Nike",
+    name: "Nike AirForce 1 Low “Heel Stitch”",
+    price: "₱1,990",
     pricevalue: 1990,
-    isNew: false
-}, {
-    image: 'Images/Shoes/New Balance 574 “Burgundy”.png',
-    detailImage: 'Images/Shoes/16 2 (1).png',
-    brand: 'New Balance',
-    name: 'New Balance 574 “Burgundy”',
-    price: '₱1,990',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/New Balance 574 “Burgundy”.png",
+    detailImage: "Images/Shoes/16 2 (1).png",
+    brand: "New Balance",
+    name: "New Balance 574 “Burgundy”",
+    price: "₱1,990",
     pricevalue: 1990,
-    isNew: false
-}, {
-    image: 'Images/Shoes/Nike Court Legacy Canvas.png',
-    detailImage: 'Images/Shoes/22 1 (1).png',
-    brand: 'Nike',
-    name: 'Nike Court Legacy Canvas',
-    price: '₱1,990',
+    isNew: false,
+  },
+  {
+    image: "Images/Shoes/Nike Court Legacy Canvas.png",
+    detailImage: "Images/Shoes/22 1 (1).png",
+    brand: "Nike",
+    name: "Nike Court Legacy Canvas",
+    price: "₱1,990",
     pricevalue: 1990,
-    isNew: false
-}, ];
+    isNew: false,
+  },
+];
 
-let productsHTML = '';
+let productsHTML = "";
 
 products.forEach((product) => {
-    productsHTML += `
-     <div class="product-wrapper" data-brand="${product.brand}" data-new="${product.isNew}">
-            <a href="shoe.html" class="js-product-link" data-product='${JSON.stringify(product)}'>
+  productsHTML += `
+     <div class="product-wrapper" data-brand="${product.brand}" data-new="${
+    product.isNew
+  }">
+            <a href="shoe.html" class="js-product-link" data-product='${JSON.stringify(
+              product
+            )}'>
                 <div class="container">
                     <div class="shoe" style="background-color: #DFDFDF;">
                         <img class="shoe-img" src="${product.image}" alt="">
@@ -221,49 +250,51 @@ products.forEach((product) => {
     `;
 });
 
-document.querySelector('.js-product-area').innerHTML = productsHTML;
+document.querySelector(".js-product-area").innerHTML = productsHTML;
 
-document.querySelectorAll('.brand-filter').forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        const selectedBrand = link.dataset.brand;
+document.querySelectorAll(".brand-filter").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const selectedBrand = link.dataset.brand;
 
-        document.querySelectorAll('.product-wrapper').forEach(product => {
-            const productBrand = product.dataset.brand;
-            product.style.display = (selectedBrand === "All" || productBrand === selectedBrand) ? "flex" : "none";
-        });
-
-        updateIntro(link.dataset.title);
+    document.querySelectorAll(".product-wrapper").forEach((product) => {
+      const productBrand = product.dataset.brand;
+      product.style.display =
+        selectedBrand === "All" || productBrand === selectedBrand
+          ? "flex"
+          : "none";
     });
+
+    updateIntro(link.dataset.title);
+  });
 });
 
-document.querySelector('.new-arrivals-filter').addEventListener('click', e => {
+document
+  .querySelector(".new-arrivals-filter")
+  .addEventListener("click", (e) => {
     e.preventDefault();
 
-    document.querySelectorAll('.product-wrapper').forEach(product => {
-        const isNew = product.dataset.new === 'true';
-        product.style.display = isNew ? 'flex' : 'none';
+    document.querySelectorAll(".product-wrapper").forEach((product) => {
+      const isNew = product.dataset.new === "true";
+      product.style.display = isNew ? "flex" : "none";
     });
 
     updateIntro(e.target.dataset.title);
-});
-
-
-function updateIntro(title) {
-    const intro = document.getElementById('intro');
-    intro.querySelector('h1').textContent = title;
-}
-
-document.querySelectorAll('.js-product-link').forEach(link => {
-    link.addEventListener('click', e => {
-        const productData = e.currentTarget.dataset.product;
-        localStorage.setItem('selectedProduct', productData);
-    });
-});
-
-shoeCard.addEventListener('click', () => {
-    localStorage.setItem('selectedProduct', JSON.stringify(product));
-    window.location.href = 'Shoe.html'; 
   });
 
-  
+function updateIntro(title) {
+  const intro = document.getElementById("intro");
+  intro.querySelector("h1").textContent = title;
+}
+
+document.querySelectorAll(".js-product-link").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    const productData = e.currentTarget.dataset.product;
+    localStorage.setItem("selectedProduct", productData);
+  });
+});
+
+shoeCard.addEventListener("click", () => {
+  localStorage.setItem("selectedProduct", JSON.stringify(product));
+  window.location.href = "Shoe.html";
+});
